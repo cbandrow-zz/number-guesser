1a8a7db7dc6845f10c0e538000befadd9d4c9b6c
Chris Bandrowsky <chris.bandrowsky@gmail.com>
2017-01-30 18:48:46 -0700

    Restructured CSS file. Commented needed functionalities in JS file.

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// Steps for completing JS.
+// 1. Create a function that chooses a random number. Use math.floor and math.random.
+// 2. Create a functioon that inputs users chosen number, then uses a conditional statement to evaluate and compare the results. This function should contain all conditionals:
+//   if too high
+//   if too low
+//   if matching
+//   if out of bounds
+//   if not a number (NaN)
+// This function should also be responsible for displaying the result in the DOM.
+// 3. Function to clear data from input box.
+// 4. Function to reset everything and load new Random number.
+// 5. Function and conditional if matching number is achieved. Adds 10 to the eligible pool of numbers.
